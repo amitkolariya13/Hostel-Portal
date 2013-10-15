672a8b4f446d5f255db74e8a3580ca3e33a0446f
amitkolariya <amitkolariya13@gmail.com>
2013-10-15 16:19:23 +0530

    Amit kolariya77

---
+++ hostel/work.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title> NITT Hostel Portal</title>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900" rel="stylesheet" />
+<link href="default.css" rel="stylesheet" type="text/css" media="all" />
+<link href="fonts.css" rel="stylesheet" type="text/css" media="all" />
+<link href="images/logo.png" rel="icon" type="image/png" />
+</head>
+<body>
+<div id="header">
+	<div id="menu" class="container">
+		<ul>
+			<li class="current_page_item"><a href="#" accesskey="1" title=""><a href =work.php>Home</a></li>
+			<li><a href="#" accesskey="1" title="">Complains</a></li>
+			<li><a href="#" accesskey="2" title=""><a href =feesreceit1.php>fees genration</a></a></li>
+			<li><a href="#" accesskey="3" title=""><a href =registration.php> Sign Up</a></a></li>
+			<li><a href="#" accesskey="4" title=""><a href =login.php>login</a></li>
+			<li><a href="#" accesskey="5" title=""><a href =./contact/contact.php>Contact Us</a></a></li>
+		</ul>
+	</div>
+</div>
+<div id="logo" class="container">
+	<h1><a href="#" class="icon icon-tasks"><span>NITT : Hostel Portal</span></a></h1>
+</div>
+<div id="page" class="container">
+	<div id="content">
+		<div class="title">
+			<h2>Welcome to our website</h2>
+		</div>
+		<p>This is <strong>a Hostel Portel</strong> which is taking care of the student complains and also provide the automatic fees generation. This site is for the conveniance.By these lots of paper work is done by online. So its keep our society clean and green.</p>
+	</div>
+	<div id="sidebar"><a href="#" class="image image-full"><img src="images/pic05.jpg" alt="" /></a></div>
+</div>
+
+<div id="contact" class="container">
+		<div class="major">
+			<h2>Get in touch</h2>
+		</div>
+		<ul class="contact">
+			<li><a href="#" class="icon icon-twitter"><span>Twitter</span></a></li>
+			<li><a href="#" class="icon icon-facebook"><span></span></a></li>
+			<li><a href="#" class="icon icon-dribbble"><span>Pinterest</span></a></li>
+			<li><a href="#" class="icon icon-tumblr"><span>Google+</span></a></li>
+			<li><a href="#" class="icon icon-rss"><span>Pinterest</span></a></li>
+		</ul>
+</div>
+</body>
+</html>
